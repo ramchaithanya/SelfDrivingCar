e0ef54145806f71040446511bf87045a059dfb4c
ramchaithanyasa@gmail.com <ramchaithanyasa@gmail.com>
2020-08-30 22:39:05 +0530

    update

--- CarND-Kidnapped-Vehicle-Project-master/src/particle_filter.cpp
+++ CarND-Kidnapped-Vehicle-Project-master/src/particle_filter.cpp
@@ -23,22 +23,15 @@ using std::string;
 using std::vector;
 using std::normal_distribution;
 
-void ParticleFilter::init(double x, double y, double theta, double std[]) {
-  /**
-   * TODO: Set the number of particles. Initialize all particles to 
-   *   first position (based on estimates of x, y, theta and their uncertainties
-   *   from GPS) and all weights to 1. 
-   * TODO: Add random Gaussian noise to each particle.
-   * NOTE: Consult particle_filter.h for more information about this method 
-   *   (and others in this file).
-   */
+void ParticleFilter::init(double x, double y, double theta, double std[]) 
+{
   if(is_initialized)
       return;
   num_particles = 101;  // TODO: Set the number of particles
 
-  normal_distribution<double> dist_x(x, std[0]);
-  normal_distribution<double> dist_y(y, std[1]);
-  normal_distribution<double> dist_theta(theta, std[2]);
+  normal_distribution<double> dist_x(0, std[0]);
+  normal_distribution<double> dist_y(0, std[1]);
+  normal_distribution<double> dist_theta(0, std[2]);
 
   for(int i =0;i<num_particles;i++)
   {
@@ -57,10 +50,9 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
-                                double velocity, double yaw_rate) {
-  /**
-   * TODO: Add measurements to each particle and add random Gaussian noise.
-   * NOTE: When adding noise you may find std::normal_distribution 
+                                double velocity, double yaw_rate) 
+{
+  /** you may find std::normal_distribution 
    *   and std::default_random_engine useful.
    *  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
    *  http://www.cplusplus.com/reference/random/default_random_engine/
@@ -76,6 +68,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
   normal_distribution<double> dist_theta(0, std_pos[2]);
   for(int i =0;i<num_particles;i++)
   {
+      
       if(fabs(yaw_rate) < 0.00001)
       {
           particles[i].x += velocity * delta_t * cos(particles[i].theta);
@@ -96,14 +89,11 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
 void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted, 
                                      vector<LandmarkObs>& observations) {
   /**
-   * TODO: Find the predicted measurement that is closest to each 
+   * Find the predicted measurement that is closest to each 
    *   observed measurement and assign the observed measurement to this 
    *   particular landmark.
-   * NOTE: this method will NOT be called by the grading code. But you will 
-   *   probably find it useful to implement this method and use it as a helper 
-   *   during the updateWeights phase.
    */
-    for (auto ob : observations)
+    for (auto &ob : observations)
     {
         int currid = -1;
         double minDist = std::numeric_limits<double>::max();
@@ -124,7 +114,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
                                    const vector<LandmarkObs> &observations, 
                                    const Map &map_landmarks) {
   /**
-   * TODO: Update the weights of each particle using a mult-variate Gaussian 
+   *  Update the weights of each particle using a mult-variate Gaussian 
    *   distribution. You can read more about this distribution here: 
    *   https://en.wikipedia.org/wiki/Multivariate_normal_distribution
    * NOTE: The observations are given in the VEHICLE'S coordinate system. 
@@ -192,7 +182,6 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
            {
                particles[i].weight *= weight;
            }
-           //std::cout<<"Ram:"<<particles[i].weight<<std::endl;
        } 
     }
 }
